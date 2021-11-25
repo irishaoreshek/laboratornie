d4676714e50d474be13eaf755ee3f858ffa6da68
Ирина Сюткина <91131404+irishaoreshek@users.noreply.github.com>
2021-11-25 19:06:59 +0300

    Add files via upload

---
+++ 6 лабораторная/7.py
@@ -0,0 +1,14 @@
+A=int(input())
+
+print("A = ",A)
+x = A*A
+print("A^2 = ",x)
+A = x*A
+print("A^3 = ",A)
+A = x*A
+print("A^5 = ",A)
+x = A*A
+print("A^10 = ",x)
+A = x*A
+print("A^15 = ",A)
+									
